6488c9f987ecceb95da1c53aebfc0eab536ce9d0
iamntoko <ntoko.ndie@iamntoko.com>
2024-12-07 14:13:57 -0600

    🔧 Install streamlit framework, create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# Welcome message
+st.title(
+""" 
++---------------------------------+
+| Welcome To Band Name Generator! |
++---------------------------------+
+""")
+
+# User input fields
+city = st.text_input("")
